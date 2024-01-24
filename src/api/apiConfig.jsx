8fe3553f7471133536db1052d60a4ce5cc43df56
Fredo-Ronan <fredocode06@gmail.com>
2024-01-24 21:13:57 +0700

    small fix not really (BETA)

--- src/api/apiConfig.jsx
+++ src/api/apiConfig.jsx
@@ -4,5 +4,5 @@ export const BASE_URL = "https://fredo-downloader-api.vercel.app";
 // export const BASE_URL = "http://localhost:3000";
 
 export const useAxios = axios.create({
-    baseURL: `${BASE_URL}/api`,
-})
+  baseURL: `${BASE_URL}/api`,
+});
